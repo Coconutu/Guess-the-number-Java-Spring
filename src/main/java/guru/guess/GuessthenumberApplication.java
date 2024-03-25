a921a2c4a23f5ac2b9ddc66119431415e6774646
Coconutu <sebastian.daniel.adrian@gmail.com>
2024-03-25 09:54:27 +0200

    First version

---
+++ src/main/java/guru/guess/GuessthenumberApplication.java
@@ -0,0 +1,14 @@
+package guru.guess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessthenumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessthenumberApplication.class, args);
+		//adaugare comentariu
+	}
+
+}
